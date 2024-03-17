240b84b89a0bff163de7e76260a54af276f95981
amal642 <mohamedamal0345@gmail.com>
2024-03-17 13:53:47 +0530

    navbar

---
+++ src/app/about/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+        <h1 className='text 2xl font-medium text-amber-600'>About</h1>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque perspiciatis alias quaerat quidem debitis aliquam aliquid nisi quam assumenda officia tempora accusamus error optio maiores, natus esse hic ducimus nesciunt sequi temporibus fuga nihil iure voluptatibus! Quaerat facilis explicabo ipsam aspernatur sint vero iusto sapiente necessitatibus quos possimus? Hic, officiis.
+        </p>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium culpa iusto nobis illo odit voluptatem quis cumque nemo veritatis ad soluta aut, repudiandae illum accusamus voluptate sequi voluptates tenetur nulla dolores eius. Ea nisi, sit, perspiciatis sapiente, qui inventore fugiat quaerat exercitationem sint deleniti odio! Voluptate asperiores in aspernatur eveniet ad doloremque nihil! Atque dolor quasi odit maiores aspernatur! At beatae temporibus placeat adipisci recusandae dicta quasi, illum iure facere, accusamus assumenda earum voluptatem sunt itaque veritatis eveniet. Est mollitia cumque rem dolorem quo minus esse deleniti voluptatibus adipisci nihil!
+        </p>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum blanditiis facere nobis consequatur iure provident culpa alias officia ducimus qui fugit amet, voluptates maxime quo quia suscipit ipsa, cum explicabo cumque quod ipsam molestias eveniet odio. Labore incidunt sunt saepe?
+        </p>
+    </div>
+  )
+}
